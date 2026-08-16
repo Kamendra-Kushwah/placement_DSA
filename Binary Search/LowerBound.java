6acfbab1d45bb6bf2645f9efd76ab20cab55c803
Kamendra Kushwah <kamendra.gla_cs23@gla.ac.in>
2026-08-16 22:53:40 +0530

    Update LowerBound.java

--- Binary Search/LowerBound.java
+++ Binary Search/LowerBound.java
@@ -1,1 +1,18 @@
 
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        // code here
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+}
